52adc2d25a3916e83aa869147cf682fa28f30b6a
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-04 18:46:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def square_array(array)
   # Return a new array of the results
   array.map {|n| n*n }
 
-  
+
 end
 
 def summon_captain_planet(planeteer_calls)
@@ -18,6 +18,6 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
+  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
 end
