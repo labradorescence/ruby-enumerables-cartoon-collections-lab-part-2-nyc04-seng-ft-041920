4d9449c9a413f2b7946710c3d31e85941ca58ade
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-04 19:11:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,8 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.map do |ele|
-    if valid_calls.include?(ele)
+    valid_calls.map do |each_calls|
+      if ele == each_calls
     return ele
   else return nil
 end
